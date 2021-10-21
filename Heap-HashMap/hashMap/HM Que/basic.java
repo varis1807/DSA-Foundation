25b9d10f952f8aefc62533ac651db22fce381edb
Varis Rana <vrvarisrana123@gmail.com>
2021-10-21 08:47:32 +0530

    commit21

--- Heap-HashMap/hashMap/HM Que/basic.java
+++ Heap-HashMap/hashMap/HM Que/basic.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 
 public class basic {
     public static void HashMapBasic() {
+        // {Key,Value}
         HashMap<String, Integer> map = new HashMap<>();
         map.put("India", 98);
         map.put("USA", 99);
         map.put("UK", 98);
-        map.put("UK", 95);   
+        map.put("UK", 95);
 
         // map.remove("UK");
         if (map.containsKey("UK"))
@@ -16,7 +17,7 @@ public class basic {
         // System.out.println(map);
         // ArrayList<String> keys = new ArrayList<>(map.keySet());
         // System.out.println(keys);
-        for(String key : map.keySet()){
+        for (String key : map.keySet()) {
             System.out.println(key + " -> " + map.get(key));
         }
 
