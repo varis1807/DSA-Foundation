f6282cf9d27719831f3572c20e7c966568dcf19b
Varis Rana <vrvarisrana123@gmail.com>
2021-10-21 08:52:21 +0530

    commit 21

--- Heap-HashMap/hashMap/HM Que/count_frequency.java
+++ Heap-HashMap/hashMap/HM Que/count_frequency.java
@@ -1,4 +1,4 @@
-import java.util.*; 
+import java.util.*;
 public class count_frequency {
     public static void frequency(String str) {
         HashMap<Character, Integer> map = new HashMap<>();
@@ -11,21 +11,20 @@ public class count_frequency {
         // }
 
         // for (int i = 0; i < str.length(); i++) {
-        //     char ch = str.charAt(i);
-        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // char ch = str.charAt(i);
+        // map.put(ch, map.getOrDefault(ch, 0) + 1);
         // }
-
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            map.putIfAbsent(ch,0);
+            map.putIfAbsent(ch, 0);
             map.put(ch, map.get(ch) + 1);
         }
-
         for (char ch : map.keySet()) {
             System.out.println(ch + " -> " + map.get(ch));
         }
     }
-
-   
- 
+    public static void main(String[] args) {
+        String str = "abdhbdhffkfifhnifjfdhhshdckskksjjhnfihfk";
+        frequency(str);
+    }
 }
