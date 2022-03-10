92db06bf83208a058b34c249c2198d7505299008
Varis Rana <vrvarisrana123@gmail.com>
2022-03-10 14:15:37 +0530

    commit 10

--- Array/ARRAY/inverse.java
+++ Array/ARRAY/inverse.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class inverse {
     public static Scanner scn = new Scanner(System.in);
-
+public static int[] arr={1,2,3};
     public static int[] input(int n) {
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
@@ -14,18 +14,20 @@ public class inverse {
     public static void inv(int[] arr) {
         int n = arr.length;
         int ans[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             int idx = arr[i];
             ans[idx] = i;
         }
-        for (int l : ans) {
-            System.out.print(l+" ");
-        }
+        // for (int l : ans) {
+        //     System.out.print(l+" ");
+        // }
     }
 
     public static void main(String[] args) {
-        int n = scn.nextInt();
-        int[] arr = input(n);
+        // int n = scn.nextInt();
+        // int[] arr = input(n);
         inv(arr);
+        for(int l:arr)
+        System.out.println(l);
     }
 }
