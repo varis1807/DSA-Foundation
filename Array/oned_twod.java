d1bcbf871a6cb9b4bd01baf0e2509216af79843f
Varis Rana <vrvarisrana123@gmail.com>
2022-04-27 19:48:57 +0530

    commit 27

--- Array/oned_twod.java
+++ Array/oned_twod.java
@@ -1,4 +1,20 @@
 import java.util.*;
 public class oned_twod {
-      
+      public static void main(String[] args){
+            int[] arr={1,2,3,4,5,6,7,8,9};
+            int n=3,m=3;
+            int [][] ans=new int[n][m];
+            for(int r=0; r<n; r++){
+                  for(int c=0; c<m; c++){
+                        ans[r][c]=arr[r*m+c];
+                  }
+            }
+
+            for(int i=0; i<n; i++){
+                  for(int j=0; j<m; j++){
+                        System.out.print(ans[i][j]+" ");
+                  }
+                  System.out.println();
+            }
+      }
 }
