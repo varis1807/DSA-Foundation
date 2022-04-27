6df6ceaa68a2c8657b46b42080be7dd34089e031
Varis Rana <vrvarisrana123@gmail.com>
2022-04-27 19:35:23 +0530

    commit 27

---
+++ Array/twod_oned.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class twod_oned {
+      public static void main(String[] args) {
+            int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int [] ans=new int[9];
+            int n=arr.length;
+            int m=arr[0].length;
+            for(int i=0; i<n; i++){
+                  for(int j=0; j<m; j++){
+                        
+                  }
+            }
+      }
+}
